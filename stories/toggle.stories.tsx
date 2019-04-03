a2e00d614272b74a22abcd47d318430572a45a02
Xavier Drdak <1198051+xdrdak@users.noreply.github.com>
2019-04-02 21:04:16 -0400

    fix toggle story

--- stories/toggle.stories.tsx
+++ stories/toggle.stories.tsx
@@ -5,15 +5,14 @@ import { storiesOf } from '@storybook/react';
 import useToggle from '../src/useToggle';
 
 const ToggleExample = () => {
-  const [ isToggled, toggle ] = useToggle();
+  const [isToggled, toggle] = useToggle();
 
   return (
     <div>
-      <button onClick={() => toggle}>Toggle</button>
+      <button onClick={toggle}>Toggle</button>
       <span>{isToggled ? '☀️' : '🌙'}</span>
     </div>
   );
 };
-
 
 storiesOf('ToggleExample', module).add('story', () => <ToggleExample />);
